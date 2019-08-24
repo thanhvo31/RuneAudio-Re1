b5bb6bf798f0fe0abc9a3d222726fe4d7cc52eb3
rern <rernrern@gmail.com>
2019-08-24 09:10:58 -0700

    Add files via upload

--- usr/local/bin/usbdac.sh
+++ usr/local/bin/usbdac.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-! systemctl -q is-active mpd && exit
+/srv/http/settings/mpdconf.sh
 
 if (( $# > 0 )); then
 	name=$( aplay -l | grep card | tail -1 | awk -F'[][]' '{print $2}' )
-	/srv/http/settings/mpdconf.sh
+	ao0=$( redis-cli get ao )
+	redis-cli mset ao0 "$ao0" ao "$name"
 else
-	sysname=$( redis-cli get ao )
-	file="/srv/http/settings/dac/$sysname"
-	[[ -e "$file" ]] && name=$( grep extlabel "$file" | cut -d: -f2- ) || name="$sysname"
+	name=$( redis-cli get ao0 )
+	redis-cli set ao "$name"
+	file="/srv/http/settings/i2s/$name"
+	[[ -e "$file" ]] && name=$( grep extlabel "$file" | cut -d: -f2- )
 fi
 
 curl -s -X POST 'http://localhost/pub?id=notify' -d '{ "title": "Audio Output Switched", "text": "'"$name"'", "icon": "output" }'
