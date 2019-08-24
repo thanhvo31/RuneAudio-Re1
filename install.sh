62d51325b43b7c2ac13b3a9e7ae2af6c1385eb26
rern <rernrern@gmail.com>
2019-08-24 21:51:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ installstart $@
 
 getinstallzip
 
-redis-cli hdel addons font lyri udac kid3 expa motd enha
+redis-cli hdel addons font lyri udac kid3 expa motd enha &> /dev/null
 
 rm -f /srv/http/{Gruntfile.js,package.json} /srv/http/assets/js/vendor/bootstrap-contextmenu*
 
