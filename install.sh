a394b1383d1866b26002e9cb9cd06bb261172c28
rern <rernrern@gmail.com>
2019-09-01 10:02:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,55 +10,6 @@ installstart $@
 
 getinstallzip
 
-# no verify ###################################
-ao=$( redis-cli get ao )
-[[ -n $ao ]] && redis-cli set audiooutput "$ao"
-redis-cli del AccessPoint activePlayer ao ao0 dirble librandom mixer_type &> /dev/null
-redis-cli hdel addons enha expa font kid3 lyri motd udac &> /dev/null
-
-rm -f /srv/http/{Gruntfile.js,package.json,lyricscontainer.php}
-rm -f /srv/http/assets/js/addonsmenu.js
-rm -f /srv/http/assets/js/vendor/{bootstrap-contextmenu*,bootstrap-select.min.js}
-
-sed -i 's/gpu_mem=16/gpu_mem=32/' /boot/config.txt
-# need verify ######################################
-if systemctl -q is-enabled hostapd; then
-	systemctl disable hostapd dnsmasq
-	redis-cli set accesspoint 1
-fi
-
-if ! grep '^mixer_type' /etc/mpd.conf; then
-	mixertype=$( grep mixer_type /etc/mpd.conf | head -1 | cut -d'"' -f2 )
-	sed -i -e '/mixer_type/ d
-	' -e '/max_connections/ a\mixer_type              "'$mixertype'"
-	' /etc/mpd.conf
-fi
-
-# fix missing locations by systemd upgrade
-mkdir -p /etc/systemd/resolved.conf.d
-echo '[Resolve]
-DNSSEC=false' > /etc/systemd/resolved.conf.d/dnssec.conf
-
-[[ -L /etc/resolv.conf ]] || ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
-systemctl restart systemd-resolved 
-
-sed -i 's|/var/run|/run|' /usr/lib/tmpfiles.d/bluealsa.conf
-sed -i 's|/var/run|/run|' /usr/lib/tmpfiles.d/samba.conf 
-
-file="$( ls -d /mnt/MPD/USB/*/ ).mpdignore"
-if [[ ! -e "$file" ]]; then
-echo 'bookmarks
-coverarts
-gpio
-lyrics
-mpd
-playlists
-redis
-tmp
-webradiopl
-webradios' > "$file"
-fi
-
 setColor
 
 installfinish $@
