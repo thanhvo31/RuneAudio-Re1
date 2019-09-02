af07892efbc0820482a1e43ffe73a958f0c2da5b
rern <rernrern@gmail.com>
2019-09-02 17:18:35 +0700

    Delete addonsdl.php

--- srv/http/addonsdl.php
+++
@@ -1,3 +0,0 @@
-<?php
-exec( '/usr/bin/sudo /usr/bin/wget -q --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/master/srv/http/addonslist.php -O /srv/http/addonslist.php', $output, $exit );
-echo $exit;
